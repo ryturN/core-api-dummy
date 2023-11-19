e494ca38a1af40113ca32651944e9046bf5b6b9f
watashiox14 <watashiox123@gmail.com>
2023-11-19 20:59:19 +0700

    make routes good to see

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,9 @@ const usersTable = require('../models/tables/usersTable.js');
 
 const router = express.Router();
 
+//  ============================= GET ROUTER ========================================== //
+
+
 router.get('/', async (req, res) => {
   const cookie = await req.cookies;
     if (!cookie.verifyToken) {
@@ -31,49 +34,49 @@ router.get('/register',(req,res)=>{
 })
 router.get('/verify',(req,res)=>{
     res.render('verify')
-})
+  })
+  router.get('/profile/:username?',profile.profileUsers);
+  router.get('/profile',profile.profiles);
+  router.get('/forget',(req,res)=>{
+    res.render('forget')
+  })
+  router.get('/dashboard',(req,res)=>{
+      const cookie = req.cookies;
+      if(!cookie.verifyToken){
+          return res.redirect('/')
+      }
+      const data = {
+          admin:true
+      };
+      res.render('home/dashboard',{data});
+  })
+  router.get('/logout',(req,res)=>{
+      res.clearCookie('verifyToken');
+      res.json({
+          status: 'success',
+          message: 'See You Later Nerd'})
+  })
+  
+  router.get('*',(req,res)=>{
+      res.status(404).json({
+          status: 'fail',
+          message: 'u got wrong address bro'
+      })
+  })
+
+
+//  ============================= POST ROUTER ========================================== //
+
 
 router.post('/register',auth.register)
 router.post('/verifyUser',auth.verify)
 router.post('/login', auth.login)
-router.get('/profile/:username?',profile.profileUsers);
 router.all('/profile/edit',profile.updateProfile);
-router.get('/profile',profile.profiles);
 router.post('/forget',resetPassword.forgetPassword);
-router.get('/forget',(req,res)=>{
-  res.render('forget')
-})
 router.post('/forget/verify', resetPassword.verifyCode)
 router.post('/forget/verify/new', resetPassword.enterNewPassword)
 
 
-router.get('/dashboard',(req,res)=>{
-    const cookie = req.cookies;
-    if(!cookie.verifyToken){
-        return res.redirect('/')
-    }
-    const data = {
-        admin:true
-    };
-    res.render('home/dashboard',{data});
-})
-
-
-
-
-router.get('/logout',(req,res)=>{
-    res.clearCookie('verifyToken');
-    res.json({
-        status: 'success',
-        message: 'See You Later Nerd'})
-})
-
-router.get('*',(req,res)=>{
-    res.status(404).json({
-        status: 'fail',
-        message: 'u got wrong address bro'
-    })
-})
 
 
 module.exports =router;
